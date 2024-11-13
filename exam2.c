000a9d1bb297b91fcfc2070c4488979535e79960
Jishan Patel <jishanpatel.15072003@gmail.com>
2024-11-13 10:06:56 +0530

    first commit

---
+++ exam2.c
@@ -0,0 +1,13 @@
+/*Q.1 Temperature Converter
+Develop a program that converts temperature from degrees Celsius to Fahrenheit using the
+formula: °F = (9/5 × °C) + 32*/
+
+#include<stdio.h>
+int main(){
+float celsius,i,j,k,c,g;
+ printf("The temperature in celsius:");
+ scanf("%f",&celsius);
+ i = (j/k*c ) + celsius);
+ printf("The temperature in Fahrenheit is %f: ",i,j);
+ 	
+}
---
+++ exam3.c
@@ -0,0 +1,19 @@
+/*Write a program to find the third angle of a right triangle when two other angles are given.
+Example:
+Input: First angle: 65, Second angle: 45
+Output: Third angle: 70*/
+
+
+
+#include<stdio.h>
+int main(){
+	int i,j,k;
+	printf("first angle:");
+	scanf("%d",&i);
+	printf("second angle:");
+	scanf("%d",&j);
+	printf("third angle :" );
+     
+
+}
+
